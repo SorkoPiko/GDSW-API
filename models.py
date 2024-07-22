991d6a4f0938b33f48dfcdd086245d87eb8d90f0
Orion Railean <orion.railean@gmail.com>
2024-07-22 10:00:18 +1000

    add new scraping function

--- models.py
+++ models.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel
+from typing import Literal
+
+
+class Route(BaseModel):
+    start: int | None
+    end: int | None
+    desc: str
+    type: Literal["normal", "minigame", "other"]
 
 
 class SecretWay(BaseModel):
-    id: int
     desc: str
     src: str
-    sw: str
+    routes: list[Route]
     yt: str | None = None
 
 
--- utils.py
+++ utils.py
@@ -10,9 +10,44 @@ def parse_data(data):
                 yt = f"https://youtu.be/{extract.video_id(i[4])}"
             except:
                 yt = None
+        if "or" in i[1].lower():
+            ways = i[1].lower().replace(' ', '').split("or")
+        else:
+            ways = i[1].lower().replace(' ', '').split(",")
+        routes = []
+        if len(ways) > 1:
+            descs = list(filter(None, i[2].split("\n")))
+            if len(ways) != len(descs):
+                print("a")
+                raise ValueError("Length of ways and descriptions do not match")
+            for j in range(len(ways)):
+                start, end = ways[j].split("-")
+                routes.append({
+                    "start": int(start),
+                    "end": int(end),
+                    "desc": descs[j],
+                    "type": "normal"
+                })
+        else:
+            if ways[0] == "minigame":
+                start, end, type_ = None, None, "minigame"
+            else:
+                try:
+                    start, end = ways[0].split("-")
+                    start, end = int(start), int(end)
+                    type_ = "normal"
+                except:
+                    start, end, type_ = None, None, "other"
+            routes.append({
+                "start": start,
+                "end": end,
+                "desc": i[2],
+                "type": type_
+            })
+
+        print(routes)
         newdata.update({i[0]: {
-            "sw": i[1],
-            "desc": i[2],
+            "routes": routes,
             "src": i[3],
             "yt": yt
         }})
